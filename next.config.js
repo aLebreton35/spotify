e9b11cdf07c7f64969d07fc7cd0c1d2168ad94c1
Adrien L <lebreton.adrien35@gmail.com>
2024-07-12 00:21:11 +0200

    Deploy PROD

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/spotify', // TODO : que en PROD je pense
+  assetPrefix: '/spotify/',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval';",
+          },
+        ],
+      },
+    ];
+  },
+
   reactStrictMode: true,
   images: {
     domains: ["zhcpytnhrbxpmfbakgzx.supabase.co"],
