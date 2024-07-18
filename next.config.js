39534133053efe15934fabfb1918dac0c2f08e0a
Adrien L <lebreton.adrien35@gmail.com>
2024-07-18 22:32:54 +0200

    Deploy : error page blanche github

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline';",
           },
         ],
       },
